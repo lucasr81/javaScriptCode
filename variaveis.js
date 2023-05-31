ca598c02b28ab1ce1e6cfac6d75b1e11f78a7a0b
Lucas <lucasr81@gmail.com>
2023-05-31 01:07:32 -0300

    Curso_JavaScrip_Basico

--- variaveis.js
+++ variaveis.js
@@ -1,3 +1,10 @@
 //Variaveis//
-let idade = 26;
+let idade = 26; // Criando variavel 
 console.log(idade);
+
+
+idade = 30; // Variavel do tipo number
+
+let numero = 50; // number
+let alutura = 1.77; // number
+let nome = "Walisson" //string
